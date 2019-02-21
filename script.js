ea21610f0b955648440c53fa7ccefb36a67b0ab1
John Black <math_bd@mail.ru>
2019-02-21 14:30:47 +0300

    Test v1.1

--- script.js
+++ script.js
@@ -1,48 +1,85 @@
-let data = {
-    buy:{
-      wmr: 0.96,
-      wmz: 55.7,
-    },
-    sell:{
-      wmr:1,
-      wmz:58.9,
-    }
-  };
-
 const app = new Vue ({
     el: '#app',
     data: {
-        message: '',
-        cps:[
-          { text: 'WMR', buy_value: data.buy.wmr, sell_value: data.sell.wmr},
-          { text: 'WMZ', buy_value: data.buy.wmz, sell_value: data.sell.wmz }
-        ],
-        buy:{
-            wmr: data.buy.wmr,
-            wmz: data.buy.wmz
+        str_buy:[],
+        str_sell:[],
+        resp:{
+            buy:{wmr: '', wmz: ''},
+            sell:{wmr: '',wmz: ''}
         },
-        sell:{
-            wmr: data.sell.wmr,
-            wmz: data.sell.wmz
-        } 
+        cps:[
+            {text: 'WMR', val: 1},
+            {text: 'WMZ', val: 2}
+        ]
     },
     methods: {
-        change_buy: function(){
+        change_buy: function(){  
+            const sel = this.$refs.select_buy[this.$refs.select_buy.selectedIndex].text.toLowerCase();           
             let cnt = 0;
-            if($('#input-buy').val()!='' && $('#input-buy').val()!=0 && $('#select-buy').val()) cnt = $('#select-buy').val() * $('#input-buy').val();
-            $('#input-buy-cnt').text(cnt);
+            if ( this.$refs.input_buy.value != ''
+                && this.$refs.input_buy.value != 0
+                && this.$refs.select_buy.selectedIndex > 0) {
+              cnt = this.resp.buy[sel] * this.$refs.input_buy.value;
+            }
+            this.$refs.label_buy_cnt.innerHTML = cnt;
+
+            this.str_buy = {'inp':this.$refs.input_buy.value, 'sel': this.$refs.select_buy.selectedIndex,'lbl': cnt};    
+            const parsed = JSON.stringify(this.str_buy);
+            localStorage.setItem('str_buy', parsed);
         },
         change_sell: function(){
+            const sel = this.$refs.select_sell[this.$refs.select_sell.selectedIndex].text.toLowerCase();
             let cnt = 0;
-            if($('#input-sell').val()!='' && $('#input-sell').val()!=0 && $('#select-sell').val()) cnt = $('#input-sell').val() / $('#select-sell').val();
-            $('#input-sell-cnt').text(cnt);
+            if ( this.$refs.input_sell.value != ''
+                && this.$refs.input_sell.value != 0
+                && this.$refs.select_sell.selectedIndex > 0) {
+              cnt = this.$refs.input_sell.value / this.resp.sell[sel] ;
+            }
+            this.$refs.label_sell_cnt.innerHTML = cnt;
+            
+            this.str_sell = {'inp':this.$refs.input_sell.value, 'sel': this.$refs.select_sell.selectedIndex,'lbl': cnt};    
+            const parsed = JSON.stringify(this.str_sell);
+            localStorage.setItem('str_sell', parsed);
         },
         isNumberKey: function(evt){
             var charCode = (evt.which) ? evt.which : evt.keyCode;
             if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57)){
-                event.preventDefault();
+                evt.preventDefault();
                 return false;
             }
         }
+    },
+    mounted() { 
+        console.log(`mounted:${localStorage.getItem('str_buy')}|${localStorage.getItem('str_sell')}`)
+        if (localStorage.getItem('str_buy')) {
+            try {
+                this.str_buy = JSON.parse(localStorage.getItem('str_buy'));
+            } catch(e) {
+                localStorage.removeItem('str_buy');
+            }
+        }
+        if (localStorage.getItem('str_sell')) {
+            try {
+                this.str_sell = JSON.parse(localStorage.getItem('str_sell'));
+            } catch(e) {
+                localStorage.removeItem('str_sell');
+            }
+        }
+    },
+    created() {
+        setTimeout(()=>{
+          data = {
+            sell:{
+              wmr: 0.96,
+              wmz: 55.7,
+            },
+            buy:{
+              wmr:1,
+              wmz:58.9,
+            }
+          }
+        //console.log(data);
+          this.resp = data;
+        },1000)
     }
 })
